54a5fdf787cbd59c6f2cb07698ef2aefa72a0aed
Daniel Garcia <albdangar@gmail.com>
2026-05-17 13:20:20 -0400

    refactor: remove middleware.ts

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import NextAuth from "next-auth"
-import authConfig from "./auth.config"
- 
-export const { auth: middleware } = NextAuth(authConfig)
-
-// Filter Middleware to avoid API routes and static assets
-export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
-};
